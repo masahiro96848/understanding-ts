f2101a0864ea15279a1c2782a294d0df4363d497
masahiro96848 <feetwcpic9436@gmail.com>
2022-07-06 22:06:35 +0900

    プロパティの初期化

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,16 @@
 "use strict";
 class Department {
-    constructor(n) {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        // private id: string;
+        // name: string;
         this.employees = [];
-        this.name = n;
+        // this.id = id;
+        // this.name = n;
     }
     describe() {
-        console.log('Department:' + this.name);
+        console.log(`Department (${this.id}): ${this.name}`);
     }
     addEmployee(employee) {
         this.employees.push(employee);
@@ -15,9 +20,9 @@ class Department {
         console.log(this.employees);
     }
 }
-const accounting = new Department('Accounting');
+const accounting = new Department('d1', 'Accounting');
 accounting.addEmployee('Max');
 accounting.addEmployee('Manu');
-accounting.employees[2] = 'Anna';
+// accounting.employees[2] = 'Anna'
 accounting.describe();
 accounting.printEmployeeInformation();
