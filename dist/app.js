3b52408273a8571b4d3baa912ace2f1b7fa1a9fa
masahiro96848 <feetwcpic9436@gmail.com>
2022-07-06 22:01:18 +0900

    private public就職し

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,23 @@
 "use strict";
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
-    console.log('clicked');
-});
+class Department {
+    constructor(n) {
+        this.employees = [];
+        this.name = n;
+    }
+    describe() {
+        console.log('Department:' + this.name);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+const accounting = new Department('Accounting');
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+accounting.employees[2] = 'Anna';
+accounting.describe();
+accounting.printEmployeeInformation();
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,32 @@
-const button = document.querySelector('button')!;
+class Department {
+    name: string;
+    private employees:  string[] = [];
 
-button.addEventListener('click', () => {
-    console.log('clicked');
-});
+    constructor(n: string) {
+        this.name = n;
+    }
+
+    describe(this: Department) {
+        console.log('Department:' + this.name);
+    }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee)
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length)
+        console.log(this.employees)
+    }
+}
+
+const accounting =  new Department('Accounting');
+
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+
+accounting.employees[2] = 'Anna'
+
+accounting.describe()
+accounting.printEmployeeInformation();
+
