fc8a3f29c83a4c555b3bb3754a6b90e89265bba6
masahiro96848 <feetwcpic9436@gmail.com>
2022-07-08 08:13:10 +0900

    インターフェース

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+interface Greetable {
+    readonly name: string;
+
+    greet(phrase: string): void;
+}
+
+class Person implements Greetable {
+    name: string;
+    age = 30;
+    constructor(n: string) {
+        this.name = n;
+    }
+
+    greet(phrase: string) {
+        console.log(phrase + ' ' + this.name)
+    }
+}
+
+let user1: Greetable;
+
+user1 = new Person('Max');
+console.log(user1)
+
+user1.greet('Hello I am');
