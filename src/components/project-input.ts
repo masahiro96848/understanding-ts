4088398cf2775976d4926e40c1eb9efeca755fe2
masahiro96848 <feetwcpic9436@gmail.com>
2022-07-10 23:06:37 +0900

    名前空間の使用とフォルダ、ファイル分け

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,22 +1,11 @@
-/// <reference path="models/drag-drop.ts" />
-/// <reference path="models/project.ts" />
-/// <reference path="state/project-state.ts" />
-/// <reference path="util/validation.ts" />
-/// <reference path="decorators/autobind.ts" />
-/// <reference path="components/project-input.ts" />
-/// <reference path="components/project-list.ts" />
+/// <reference path="base-component.ts" />
+/// <reference path="../decorators/autobind.ts" />
+/// <reference path="../util/validation.ts" />
+/// <reference path="../state/project-state.ts" />
 
 namespace App {
-    
-
-    
-
-    
-
-    
-
     // ProjectInput Class
-    class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+    export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
         titleInputElement: HTMLInputElement;
         describeInputElement: HTMLInputElement;
         mandayInputElement: HTMLInputElement;
@@ -87,9 +76,7 @@ namespace App {
             }
         }
     }
-
-    new ProjectInput();
-    new ProjectList('active');
-    new ProjectList('finished');
 }
+
+
 
