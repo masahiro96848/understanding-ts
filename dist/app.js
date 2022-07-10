8fb2dabb015d825271614f771788a598340dca3b
masahiro96848 <feetwcpic9436@gmail.com>
2022-07-10 10:54:45 +0900

    プロジェクト一覧の作成

--- dist/app.js
+++ dist/app.js
@@ -30,6 +30,27 @@ function autobind(_, _2, descriptor) {
     };
     return adjDescriptor;
 }
+// ProjectList Class
+class projectList {
+    constructor(type) {
+        this.type = type;
+        this.templateElement = document.getElementById('project-list');
+        this.hostElement = document.getElementById('app');
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
+    renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector('ul').id = listId;
+        this.element.querySelector('h2').textContent = this.type === 'active' ? '実行中プロジェクト' : '完了プロジェクト';
+    }
+}
 // ProjectInput Class
 class ProjectInput {
     constructor() {
@@ -99,3 +120,5 @@ __decorate([
     autobind
 ], ProjectInput.prototype, "submitHandler", null);
 const prjInput = new ProjectInput();
+const activePrjList = new projectList('active');
+const finishedPrjList = new projectList('finished');
--- src/app.ts
+++ src/app.ts
@@ -39,6 +39,33 @@ function autobind(
         return adjDescriptor;
     }
 
+// ProjectList Class
+class projectList {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLElement;
+    constructor(private type: 'active' | 'finished') {
+        this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild as HTMLElement;
+        this.element.id = `${this.type}-projects`
+        this.attach();
+        this.renderContent();
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
+
+    private renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector('ul')!.id = listId;
+        this.element.querySelector('h2')!.textContent = this.type === 'active' ? '実行中プロジェクト' : '完了プロジェクト';
+    }
+}
+
 // ProjectInput Class
 class ProjectInput {
     templateElement: HTMLTemplateElement;
@@ -125,3 +152,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new projectList('active');
+const finishedPrjList = new projectList('finished');
